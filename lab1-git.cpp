853c03eb5d8a0f0d54448d2991f23f731d5db847
Artem Vazhev <vazhev_artem@mail.ru>
2026-09-06 13:23:50 +0700

    Первый коммит: меню и первая функция варианта 71

---
+++ lab1-git.cpp
@@ -0,0 +1,45 @@
+﻿// Лабораторная работа № 1. Вариант 71.
+// Выполнил: Важев А.Е., группа ПИ-52.
+#include "lab1-git.h"
+
+// ===== Расчётные функции варианта =====
+
+// Перевод секунд в часы
+int hoursOf(int sec) {
+	return sec / 3600;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int choice;
+	int sec;
+
+	do {
+		std::cout << "\n=== Вариант 71: Перевод секунд в часы;минуты;секунды ===\n";
+
+		std::cout << "1. Секунды => часы\n";
+		std::cout << "0. Выход\n";
+		std::cout << "Выберите пункт: ";
+
+		std::cin >> choice;
+		switch (choice) {
+		case 1: {
+			std::cout << "Введите количество секунд: ";
+			std::cin >> sec;
+			std::cout << "В " << sec << " секундах " << hoursOf(sec) << " часов.\n";
+			break;
+		}
+		case 0:
+			std::cout << "Работа завершена.\n";
+			break;
+		default:
+			std::cout << "Такого пункта нет.\n";
+		}
+
+	} while (choice != 0);
+
+	return 0;
+
+}
